f976e89ae105236f005b9e371705ed9f61b5cc24
NickThys3012 <nick.thys@vlaanderen.be>
2026-06-26 22:28:38 +0200

    US-1.2: Web design almost done

---
+++ FosterFlow/FosterFlow.Contracts/DTOs/Auth/RegisterShelterRequest.cs
@@ -0,0 +1,13 @@
+namespace FosterFlow.Contracts.DTOs.Auth;
+
+public class RegisterShelterRequest
+{
+    public string Name { get; set; } 
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string? Phone { get; set; }
+    public string? Street { get; set; }
+    public string? PostalCode { get; set; }
+    public string? City { get; set; }
+    public string? Country { get; set; }
+}
--- FosterFlow/FosterFlow.Web/Pages/Signin/Components/ShelterSigninForm.razor.cs
+++ FosterFlow/FosterFlow.Web/Pages/Signin/Components/ShelterSigninForm.razor.cs
@@ -1,6 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using FosterFlow.Contracts.DTOs.Auth;
 using Microsoft.AspNetCore.Components;
 
 namespace FosterFlow.Web.Pages.Signin.Components;
 
-public partial class ShelterSigninForm : ComponentBase {}
+public partial class ShelterSigninForm : ComponentBase
+{
+    private readonly RegisterShelterRequest _model = new();
+    private string?      _serverError;
+    private bool         _loading;
 
+    private async Task HandleLogin()
+    {
+        // EditForm only calls this when FluentValidation passes
+        _loading     = true;
+        _serverError = null;
+        Thread.Sleep(1000);
+        
+        _loading     = false;
+        _serverError = null;
+    }
+
+}
