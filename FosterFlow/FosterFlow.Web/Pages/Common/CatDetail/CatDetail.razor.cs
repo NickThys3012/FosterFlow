f60e1707a2ee7789c90b1d82f62b2d08701961aa
NickThys3012 <nick.thys@vlaanderen.be>
2026-07-01 17:58:42 +0200

    fix last one

--- FosterFlow/FosterFlow.Web/Pages/Common/CatDetail/CatDetail.razor.cs
+++ FosterFlow/FosterFlow.Web/Pages/Common/CatDetail/CatDetail.razor.cs
@@ -1,6 +1,10 @@
+using System.Security.Claims;
 using FosterFlow.Contracts.DTOs.Cats;
 using FosterFlow.Web.Services.HttpServices;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+
 namespace FosterFlow.Web.Pages.Common.CatDetail;
 
 public partial class CatDetail : ComponentBase
@@ -10,6 +14,10 @@ public partial class CatDetail : ComponentBase
     private CatDto? _catDto;
 
     private bool _catNotFound;
+
+    [Inject] private AuthenticationStateProvider AuthStateProvider { get; set; } = default!;
+    [Inject] private IJSRuntime Js { get; set; } = default!;
+
     public CatDetail(NavigationManager nav, CatService cat)
     {
         _nav = nav;
@@ -31,8 +39,35 @@ public partial class CatDetail : ComponentBase
         _catNotFound = true;
     }
 
-    private void ReturnToCats()
+    private async Task ReturnToCats()
     {
-        _nav.NavigateTo("/Shelter/Dashboard");
+        try
+        {
+            // First, try to go back in browser history
+            var navigated = await Js.InvokeAsync<bool>("navigation.goBack");
+            if (navigated)
+            {
+                return;
+            }
+        }
+        catch
+        {
+            // If JS interop fails, fall through to default navigation
+        }
+
+        // Fall back to role-based navigation
+        var authState = await AuthStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+        var role = user.FindFirst(ClaimTypes.Role)?.Value ?? string.Empty;
+
+        var destination = role switch
+        {
+            "Foster" => "/Foster/Dashboard",
+            "Shelter" => "/Shelter/Dashboard",
+            "Admin" => "/Admin/Dashboard",
+            _ => "/"
+        };
+
+        _nav.NavigateTo(destination);
     }
 }
